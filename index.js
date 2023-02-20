f46de17bbf8255eee902196941622490219e15ad
j3553pat <jessecodedev@gmail.com>
2023-02-20 10:35:29 -0600

    done

--- index.js
+++ index.js
@@ -17,38 +17,62 @@ let finalWord = [["hello"],
 ["price"],
 ["past"]]
 
-let selectWord = ""
 let chancesLeft = 6
 let guessWord = [];
 
-let item = finalWord[Math.floor(Math.random()*finalWord.length) + 1];
+let item = finalWord[Math.floor(Math.random()*finalWord.length)];
 
-console.log(item)
+const gameBoard = () => {
+    if (guessWord.length ===0) {
+    for (let i = 0; i < item.length; i++) {
+        guessWord.push("_")
+    } }
+    console.log(guessWord.join(""))
+}
 
-const userInput = (letter) => {
- let ifRight = false
- for(let i = 0; i < selectWord.length; i++) {
-    if(selectWord[i] === letter) {
-        ifRight= true
-        guessWord[i] = letter 
-    }
- }
- return ifRight
-};
+const userInput = (x) => {
+
+    for(let i = 0; i < item.length; i++) {
+        if(x === item[i]) {
+            guessWord[i] = x
+        }
+     } 
+        if(item.includes(x) == false) {
+            console.log("sorry, try again")
+            chancesLeft--
+            console.log(`lives: ${chancesLeft}`)
+        } 
+        gameBoard()
+    };
 
 const checkForWIn = () => {
-    return guessWord.join('') === selectWord
+    if (!guessWord.includes("_")) {
+        console.log("you won Hangman!")
+    } else if (chancesLeft===0){
+        console.log("You lose :(. Crtl + C and enter 'node index.js' to try again.")
+    }
 }
 
-let turn = function () {
-    console.log()
+const startGame = (x) => {
+    userInput(x)
+    checkForWIn()
 }
+gameBoard()
 
-const execute = () => {
-    console.log('Lets Play!')
-    selectWord()
-    console.log('The word is: ' + guessWord.join(''))
+
+
+const getPrompt = () => {
+	rl.question('type a letter guess: ', (x) => {
+		startGame(x);
+		getPrompt();
+	}); 
+};
+if (typeof describe === 'function') {
+	describe('startGame()', () => {
+	    it('should be of type function', () => {
+			assert.equal(typeof startGame, 'function');
+		});
+	});
+} else {
+	getPrompt();
 }
-
-
-
